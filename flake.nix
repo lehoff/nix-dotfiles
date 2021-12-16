53044fd242e9e5fdc0a7b0e5c62d74abfd8ae071
Josep Lluis Giralt D'Lacoste <pep.g.dlacoste@erlang-solutions.com>
2021-12-16 10:24:10 +0100

    nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Torben's darwin system";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-20.09-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
