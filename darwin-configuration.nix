9f5eca9086e7335c5da84f8cb725be7ee435b269
Torben Hoffmann <lehoff@protonmail.com>
2021-12-28 10:22:53 +0100

    Works after total clean

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -1,6 +1,40 @@
 { config, lib, pkgs, ... }:
 
+with lib;
+
+let
+  # from https://github.com/LnL7/nix-darwin/blob/master/modules/system/defaults-write.nix
+  isFloat = x: isString x && builtins.match "^[+-]?([0-9]*[.])?[0-9]+$" x != null;
+
+  boolValue = x: if x then "YES" else "NO";
+
+  writeValue = value:
+    if isBool value then "-bool ${boolValue value}" else
+    if isInt value then "-int ${toString value}" else
+    if isFloat value then "-float ${toString value}" else
+    if isString value then "-string '${value}'" else
+    throw "invalid value type";
+ 
+  writeDefault = domain: key: value:
+    "defaults write ${domain} '${key}' ${writeValue value}";
+in
 {
+
+  system = {
+    defaults = {
+      dock = {
+        orientation = "right"; # remember to do a `killall Dock` if install.sh does not
+        };
+  #    NSGlobalDomain = {
+  #      "com.apple.trackpad.TrackpadOrientationMode" = 1;
+  #    };
+    };
+  };
+
+  # Does not work atm. Done in install.sh instead.
+  #writeDefault "com.freron.MailMate" MmMessagesOutlineMoveStrategy "previous";
+
+  # Nix
   services.nix-daemon.enable = true;
 
   nix = {
@@ -21,11 +55,11 @@
 
   environment = {
     shells = [ pkgs.zsh ];
-    systemPackages = [ 
-      pkgs.zsh
-      pkgs.gcc
-      pkgs.ripgrep
-      pkgs.speedtest-cli
+    systemPackages = with pkgs; [ 
+      zsh
+      gcc
+      ripgrep
+      speedtest-cli
     ];
   };
 
@@ -41,17 +75,17 @@
 
   };
 
-  programs = {
-    bash.enable = false;
-    zsh.enable = true;
-  };
 
   homebrew = {
     brewPrefix = "/opt/homebrew/bin";
     enable = true;
     global.brewfile = true;
 
-    taps = [ "homebrew/core" "homebrew/cask" ];
+    taps = [ 
+      "homebrew/core"
+      "homebrew/cask" 
+      "homebrew/cask-drivers"
+    ];
 
     casks = [
       "bettertouchtool"
@@ -75,6 +109,49 @@
       "xmind"
       "zoom"
     ];
+  };
+
+  imports = [ <home-manager/nix-darwin> ];
+
+  home-manager.users.lehoff = { pkgs, ... }: {
+    home = {
+      packages = with pkgs; [ 
+        tree 
+        yq
+        jq
+        direnv
+      ];
+    };
+    programs.zsh = {
+      enable = true;
+      shellAliases = {
+        ll = "ls -l";
+      };
+      #plugins = [
+      #{
+      #  name = "zsh-nix-shell";
+      #  file = "nix-shell.plugin.zsh";
+      #  src = pkgs.fetchFromGitHub {
+      #    owner = "chisui";
+      #    repo = "zsh-nix-shell";
+      #    rev = "v0.4.0";
+      #    sha256 = "037wz9fqmx0ngcwl9az55fgkipb745rymznxnssr3rx9irb6apzg";
+      #  };
+      #}
+      #];
+      initExtra = ''
+        ${builtins.readFile "/Users/lehoff/nix-dotfiles/dot-zshrc"}
+      '';
+    };
+    programs.direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+      enableZshIntegration = true;
+    };
+  };
+
+  programs = {
+    bash.enable = false;
   };
 
 }
--- install.sh
+++ install.sh
@@ -1,24 +1,46 @@
 #!/bin/sh
+PATH=/Users/lehoff/.nix-profile/bin:/nix/var/nix/profiles/default/bin:$PATH
 
 ln -sf ${PWD}/nix-channels ~/.nix-channels
+
+#from https://github.com/NixOS/nixpkgs/issues/149791#issuecomment-994964166
+#fixes file 'nixpkgs' was not found in the Nix search path
+export NIX_PATH=${NIX_PATH:+$NIX_PATH:}$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels
+
+export NIX_PATH=darwin=$HOME/.nix-defexpr/channels/darwin:$NIX_PATH
 export NIX_PATH=home-manager=$HOME/.nix-defexpr/channels/home-manager:$NIX_PATH
 export NIX_PATH=darwin-config=$HOME/.nixpkgs/darwin-configuration.nix:$HOME/.nix-defexpr/channels:$NIX_PATH
 
+echo "Current environment:"
+env |grep -i ^nix
+
+echo "NEXT: channel update"
 nix-channel --update
 
-# nix-shell '<home-manager>' -A install 
+echo "NEXT: install home-manager"
+nix-shell '<home-manager>' -A install 
 
+echo "NEXT: linking configuration files..."
 mkdir -p ~/.nixpkgs
 ln -sf ${PWD}/config.nix ~/.nixpkgs/
 ln -sf ${PWD}/nix.conf ~/.nixpkgs/
 ln -sf ${PWD}/darwin-configuration.nix ~/.nixpkgs/
+ln -sf ${PWD}/home.nix ~/.nixpkgs/
 
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
+echo "NEXT: build darwin"
 nix-build '<darwin>' -A installer --out-link /tmp/nix-darwin && /tmp/nix-darwin/bin/darwin-installer
+echo "NEXT: darwin-rebuild switch"
+/run/current-system/sw/bin/darwin-rebuild switch
 
+# ensure chages take effect…
+echo "NEXT: As we modify the Dock settings we restart it..."
+killall Dock 
 
+# @TODO: find a way to do this with nix
+defaults write com.freron.MailMate MmMessagesOutlineMoveStrategy -string "previous"
+defaults write com.apple.trackpad.orientation TrackpadOrientationMode 1
 
-#nix-env -iA nixpkgs.nixFlakes
-#nix build ./\#darwinConfigurations.mimer.system
-#./result/sw/bin/darwin-rebuild switch --flake ./
+# HOME MANAGER
+#nix-shell '<home-manager>' -A install
+#home-manager switch
+#/run/current-system/sw/bin/darwin-rebuild switch
