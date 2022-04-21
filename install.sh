06ac848b5ee1683099914519972ad8da0c82b42a
Torben Hoffmann <lehoff@protonmail.com>
2022-04-21 16:00:04 +0200

    WIP getting flakes to work

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 mkdir -p ~/.nixpkgs
 
+
 # Configure the channels
 
 if ! grep -q nix-darwin ~/.nix-channels; then
@@ -9,7 +10,7 @@ if ! grep -q nix-darwin ~/.nix-channels; then
 fi
 
 export NIX_PATH=darwin=$HOME/.nix-defexpr/channels/darwin:$NIX_PATH
-export NIX_PATH=darwin-config=$HOME/Repos/dot-files/darwin-configuration.nix:$HOME/.nix-defexpr/channels:$NIX_PATH
+export NIX_PATH=darwin-config=$HOME/nix-dotfiles/darwin-configuration.nix:$HOME/.nix-defexpr/channels:$NIX_PATH
 
 if ! grep -q home-manager ~/.nix-channels; then
   echo "https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager" >> ~/.nix-channels
@@ -20,7 +21,8 @@ export NIX_PATH=home-manager=$HOME/.nix-defexpr/channels/home-manager:$NIX_PATH
 nix-channel --update
 
 nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
+relink-result-dir
 ./result/bin/darwin-installer
 
-darwin-rebuild build --flake ./\#mimer
-darwin-rebuild switch --flake ./\#mimer
+/run/current-system/sw/bin/darwin-rebuild build --flake ./\#mimer
+/run/current-system/sw/bin/darwin-rebuild switch --flake ./\#mimer
